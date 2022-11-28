c9179212bdca422625dbc7758bcfd035ae8c0b3a
gtaggana <AYGlazkov@stud.kpfu.ru>
2022-11-28 13:09:18 +0300

    Start write server execution

--- include/Server.hpp
+++ include/Server.hpp
@@ -8,6 +8,53 @@
 # include <signal.h>
 # include <time.h>
 
+
+# define MIME_PATH			"./config/mime.conf"
+# define MAX_BUFFER_RECV	65535	// max possible read
+# define MAX_LISTEN			1024	// number of listen
+# define TIMEOUT 			100		// milisecond
+# define TIMEOUT_USER 		30		// second
+# define RETRY_TO_SEND 		5		// try to send times
+
+
+# define ERROR_MIME_OPEN					"MIME ERROR: No file in : "
+# define ERROR_MIME_SEMIKOLON				"MIME ERROR: No semikolon at the end : "
+
+# define ERROR_SERVER_SOCKET				"SERVER ERROR: socket() failed"
+# define ERROR_SERVER_SETSOCKOPT			"SERVER ERROR: setsockopt() failed"
+# define ERROR_SERVER_FCNTL					"SERVER ERROR: fcntl() failed"
+# define ERROR_SERVER_BIND					"SERVER ERROR: binding connection. Socket has already been establishng"
+# define ERROR_SERVER_LISTEN				"SERVER ERROR: Listen on web port through socket file descriptor failed"
+# define ERROR_SERVER_POLL					"SERVER ERROR: POLL error"
+# define ERROR_SERVER_NOSERVER				"SERVER ERROR: NO SERVER"
+# define SERVER_START						"!!!SERVER START!!!"
+# define SERVER_WARNING						"!!!SERVER DONT START!!!"
+
+# define SERVER_POOL_WAIT					"\rWaiting connection"
+# define SERVER_POOL_WAIT_MINUS_ONE			"\rPOLL ERROR: poll = -1  "
+# define SERVER_POOL_WAIT_CONNECTION		"Recieved connection"
+# define SERVER_POOLIN_RECIEVED_SERV		"_PollInServ : "
+# define SERVER_POOLIN_RECIEVED_USER		"_PollInUser : "
+# define SERVER_POOLOUT_RECIEVED			"_PollOut"
+# define SERVER_POOLERR_RECIEVED			"_PollElse : "
+
+# define ERROR_SERVER_POOLIN_NEW_CONN		"\tACCEPT ERROR: could not accept new connection"
+# define ERROR_SERVER_POOLIN_FCNTL			"\tFCNTL ERROR: could not do fcntl()"
+# define SERVER_POOLIN_NEW_CONN				"\tNew incoming connection on fd : "
+
+# define ERROR_SERVER_POOLIN_USER_READ		"\tError read from : "
+# define SERVER_POOLIN_USER_SESS_END		"\tClient ended session from : "
+# define SERVER_POOLIN_USER_READ_END		"\tOK Reading ended from : "
+
+# define ERROR_SERVER_POOLIN_USER_SEND		"\tError send from : "
+# define SERVER_POOLOUT_USER_SEND_END		"\tOK Sending ended from : "
+# define SERVER_POOLOUT_CLOSE				"\tConnection CLOSE."
+# define SERVER_POOLOUT_NOT_CLOSE			"\tConnection NOT close."
+
+# define SERVER_POLLNVAL					"Socket was not created"
+# define SERVER_POLLHUP						"Socket broken connection"
+# define SERVER_POLLERR						"Error occurred with socket"
+
 namespace webserver {
 
 	class Server {
--- src/Server.cc
+++ src/Server.cc
@@ -1,4 +1,5 @@
 #include "../include/Server.hpp"
+#include <fstream>
 
 namespace {
 	void signal_handler(int signal) {
@@ -9,19 +10,81 @@ namespace {
 
 namespace webserver {
 
+	void Server::createHttpCodeList() {
+		http_code_list_["200"] = "OK";
+		http_code_list_["201"] = "Created";
+		http_code_list_["204"] = "No Content";
+		http_code_list_["301"] = "Moved Permanently";	
+		http_code_list_["400"] = "Bad Request";
+		http_code_list_["403"] = "Forbidden";
+		http_code_list_["404"] = "Not Found";
+		http_code_list_["405"] = "Method Not Allowed";
+		http_code_list_["411"] = "Length Required";
+		http_code_list_["413"] = "Payload Too Large";
+		http_code_list_["414"] = "URI Too Long";
+		http_code_list_["500"] = "Internal Server Error";
+		http_code_list_["502"] = "Bad Gateway";
+		http_code_list_["504"] = "Gateway Timeout";
+		http_code_list_["505"] = "HTTP Version Not Supported";
+		http_code_list_["508"] = "Loop Detected";
+	}
+
+
+	void Server::clearBuffer(std::string &buffer) {
+		int n = buffer.find("#");
+		if (n != std::string::npos) {
+			buffer = buffer.substr(0, n);
+		}
+		for (int i = buffer.size() - 1; i >= 0; i--) {
+			if (isspace(buffer[i])) {
+				buffer[i] = ' ';
+			}
+			if (isspace(buffer[i]) && (i == 0 || isspace(buffer[i - 1]) || i == buffer.size() - 1)) {
+				buffer.erase(i, 1);
+			}
+		}
+		if (buffer.back() != ';') {
+			throw std::runtime_error(ERROR_MIME_SEMIKOLON + buffer);
+		}
+		buffer.pop_back();
+	}
+
+	void Server::createMimeExt() {
+		std::ifstream mime_read(MIME_PATH);
+		if (!mime_read.is_open()) {
+			throw std::runtime_error(ERROR_MIME_OPEN + std::string(MIME_PATH));
+		}
+		std::string buffer;
+		while(!mime_read.eof()) {
+			std::getline(mime_read, buffer);
+			clearBuffer(buffer);
+			if(!buffer.empty()) {
+				std::vector<std::string> tmp_mime = split(buffer, " ");
+				for (size_t i = 1, end =tmp_mime.size(); i < end; ++i) {
+					mime_ext_list_.insert(std::pair<std::string, std::string>("." + tmp_mime[1], tmp_mime[0]));
+				}
+			}
+		}
+		mime_read.close();
+	}
+
 	Server::Server(int argc, char** argv) {
 		try {
 			signal(SIGPIPE, SIG_IGN);
 			signal(SIGINT, signal_handler);
 			signal(SIGQUIT, signal_handler);
 			signal(SIGTSTP, signal_handler);
-
+			
 			Nginx	nginx(argc, argv);
- //TODO
+			
+			createHttpCodeList();
+			createMimeExt();
+			
+			serverStart(nginx); //TODO
 		} catch(const std::exception& e) {
 			std::cerr << e.what() << '\n';
+			exit(EXIT_FAILURE);
 		}
-		
 	}
-
+	
 } //namespace webserver
